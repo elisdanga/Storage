d3645fa27258007684ecce2a148ec664feb14b6c
elisdanga <elisdanga00@gmail.com>
2020-06-09 02:58:10 +0300

    Fixed FileSystem

--- FileSystem.cpp
+++ FileSystem.cpp
@@ -8,6 +8,7 @@ bool FileSystem::read(std::string file_name, std::string& output)
 		while (this->stream.good()) {
 			getline(this->stream, line);
 			output.append(line);
+			output.append("\n");
 		}
 		this->stream.close();
 	}
@@ -20,7 +21,7 @@ bool FileSystem::read(std::string file_name, std::string& output)
 
 bool FileSystem::write(std::string file_name, std::string& input)
 {
-	this->stream.open(file_name, std::ios::in | std::ios::out | std::ios::app);
+	this->stream.open(file_name, std::ios::in | std::ios::out | std::ios::trunc); //trunc - delete -> save
 	if (this->stream.is_open()) {
 		this->stream << input;
 		this->stream.close();
--- FileSystem.h
+++ FileSystem.h
@@ -12,4 +12,3 @@ public:
 private:
 	std::fstream stream;
 };
-
