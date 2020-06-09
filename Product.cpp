e1e47ff0fa30cb43a7027a567b7e8e8c03f565a3
elisdanga <elisdanga00@gmail.com>
2020-06-09 03:05:15 +0300

    Add setters, change quantity, load and remove products

--- Product.cpp
+++ Product.cpp
@@ -5,7 +5,7 @@ Product::Product()
 {
 }
 
-Product::Product(std::string name, std::string expiring_date, std::string entry_date, std::string producer, std::string unit, double quantity, std::string location, std::string comment)
+Product::Product(std::string name, std::string expiring_date, std::string entry_date, std::string producer, std::string unit, std::string quantity, std::string location, std::string comment)
 {
 	this->name = name;
 	this->expiring_date = expiring_date;
@@ -38,7 +38,7 @@ std::string Product::get_unit() {
 	return this->unit;
 }
 
-double Product::get_quantity() {
+std::string Product::get_quantity() {
 	return this->quantity;
 }
 
@@ -50,8 +50,44 @@ std::string Product::get_comment() {
 	return this->comment;
 }
 
-void Product::set_comment(std::string)
+void Product::set_name(std::string name)
 {
+	this->name = name;
+}
+
+void Product::set_expiring_date(std::string expiring_date)
+{
+	this->expiring_date = expiring_date;
+}
+
+void Product::set_entry_date(std::string entry_date)
+{
+	this->entry_date = entry_date;
+}
+
+void Product::set_producer(std::string producer)
+{
+	this->producer = producer;
+}
+
+void Product::set_unit(std::string unit)
+{
+	this->unit = unit;
+}
+
+void Product::set_quantity(std::string quantity)
+{
+	this->quantity = quantity;
+}
+
+void Product::set_location(std::string location)
+{
+	this->location = location;
+}
+
+void Product::set_comment(std::string comment)
+{
+	this->comment = comment;
 }
 
 void Product::add(Product p, std::string& buffer)
@@ -65,7 +101,6 @@ Product* Product::create_from_file(std::string buffer)
 	std::cout << buffer;
 	return nullptr;
 }
-//todo: setters
 std::string Product::toString(Product p)
 {
 	std::string line;
@@ -84,7 +119,7 @@ std::string Product::toString(Product p)
 	line.append(p.get_unit());
 	line.append(" ");
 
-	line.append(std::to_string(p.get_quantity()));
+	line.append(p.get_quantity());
 	line.append(" ");
 
 	line.append(p.get_location());
@@ -93,14 +128,13 @@ std::string Product::toString(Product p)
 	line.append("\"");
 	line.append(p.get_comment());
 	line.append("\"");
-
-	line.append("\n");
-
+	
 	return line;
 }
 
 Product Product::fromString(std::string input)
 {
+
 	Product p;
 	std::string params[8];
 	int delimiter_pos = 0;
@@ -128,9 +162,49 @@ Product Product::fromString(std::string input)
 	p.set_entry_date(params[2]);
 	p.set_producer(params[3]);
 	p.set_unit(params[4]);
-	p.set_quantity(std::stod(params[5])); //stod: String -> Double
+	p.set_quantity(params[5]);
 	p.set_location(params[6]);
 	p.set_comment(params[7]);
 
 	return p;
+}
+
+void Product::load_products(std::string buffer, Product* pr_array)
+{
+	int n = 0;
+	int pos = 0;
+	//std::cout << buffer.find("\n");
+	//while ((pos = buffer.find("\n")) != std::string::npos) {
+	while (!buffer.empty()) {
+		pos = (buffer.find("\n") != -1) ? buffer.find("\n") : buffer.size();
+		std::string line = buffer.substr(0, pos);
+		pr_array[n] = fromString(line);
+		buffer.erase(0, pos + 1);
+		n++;
+	}
+}
+void Product::remove(std::string& buffer, std::string product_name)
+{
+	Product tmp_product;
+	std::string tmp_buffer = buffer;
+	std::string new_buffer;
+	int pos = 0;
+
+	while (!tmp_buffer.empty())
+	{
+		pos = (tmp_buffer.find("\n") != -1) ? tmp_buffer.find("\n") : tmp_buffer.size();
+		std::string line = tmp_buffer.substr(0, pos);
+		tmp_product = fromString(line);
+		if (tmp_product.get_name().compare(product_name) == 0) {
+
+		}
+		else {
+			new_buffer.append(line);
+			new_buffer.append("\n");
+		}
+		tmp_buffer.erase(0, pos + 1);
+
+	}
+	// new_buffer.pop_back();
+	buffer = new_buffer;
 }
--- Product.h
+++ Product.h
@@ -9,7 +9,7 @@ private:
 	std::string entry_date;
 	std::string producer;
 	std::string unit;
-	double quantity = 0;
+	std::string quantity;
 	std::string location;
 	std::string comment;
 public:
@@ -20,7 +20,7 @@ public:
 		std::string entry_date,
 		std::string producer,
 		std::string unit,
-		double quantity,
+		std::string quantity,
 		std::string location,
 		std::string comment
 	);
@@ -30,7 +30,7 @@ public:
 	std::string get_entry_date();
 	std::string get_producer();
 	std::string get_unit();
-	double get_quantity();
+	std::string get_quantity();
 	std::string get_location();
 	std::string get_comment();
 
@@ -39,12 +39,14 @@ public:
 	void set_entry_date(std::string);
 	void set_producer(std::string);
 	void set_unit(std::string);
-	void set_quantity(double);
+	void set_quantity(std::string);
 	void set_location(std::string);
 	void set_comment(std::string);
 
 	void add(Product p, std::string& buffer);
+	static void remove(std::string& buffer, std::string product_name);
 	static Product* create_from_file(std::string buffer);
 	static std::string toString(Product p);
 	static Product fromString(std::string);
+	static void load_products(std::string buffer, Product* pr_array);
 };
